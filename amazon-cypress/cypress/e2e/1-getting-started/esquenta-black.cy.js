286b204e639d9792eda4dfb286cf307d66338363
kaiquefaconi <kaique.faconi@sptech.school>
2024-11-06 15:02:54 -0300

    alteração no teste esquenta black

--- amazon-cypress/cypress/e2e/1-getting-started/esquenta-black.cy.js
+++ amazon-cypress/cypress/e2e/1-getting-started/esquenta-black.cy.js
@@ -4,10 +4,10 @@ describe('Amazon black friday', () => {
     cy.contains('a', 'Esquenta Black Friday').click();
     cy.get('[data-testid="filter-bubble-deals-todasofertas"]').click();
     cy.get('[data-testid="filter-departments-18991080011"] > label > .a-label > .a-size-base').click();
-    cy.get('[data-testid="B001P4SEXA"]').click();
-    cy.get('#a-autoid-0-announce').click();
-    cy.get('#quantity_4').click();
-    cy.get('#buy-now-button').click();
-    
+    cy.get('[data-testid="B07HDSL78H"]').click();
+    cy.get('#a-autoid-1-announce').click();
+    cy.get('#rcxsubsQuan_3').click();
+    cy.get('#rcx-subscribe-submit-button-announce').click();
+    cy.get('#sc-buy-box-ptc-button > .a-button-inner > .a-button-input').click();
   })
 })
